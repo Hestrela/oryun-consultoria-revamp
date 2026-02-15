ce84c1adacc5ed8d5de37c21911244ebc491c462
Hestrela <heitorestrela13@gmail.com>
2026-02-15 20:01:28 -0300

    feat: added some icons and php files

--- index.php
+++ index.php
@@ -1,40 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt_BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src/css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&family=Libre+Franklin:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=League+Spartan..." rel="stylesheet">
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <title>Oryun Consultoria</title>
-</head>
+<?php
+    
+    include 'src/componentes/header.php'
+    
+?>
 <body>
-
-    <header class="header">
-        <a href="index.php" class="nav_logo">
-            <img src="src/img/Oryun Consultoria logo horizontal.png" alt="Oryun Consultoria">
-        </a>
-
-        <nav class="nav_buttons">
-            <ul>
-                <li><a href="#">Início</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#">Fale Conosco</a></li>
-            </ul>
-        </nav>
-
-        <div class="nav_social_icons">
-            <a href="mailto:oryunconsultoria@gmail.com"><img width="35" height="35" src="https://img.icons8.com/color/48/circled-envelope.png" alt="circled-envelope"/></a>
-            <a href="tel:1126626488"><img width="" height="35" src="https://img.icons8.com/color/48/phone.png" alt="phone"/></a>
-            <a href="https://www.instagram.com/oryun_consultoria"><img width="35" height="35" src="https://img.icons8.com/color/48/instagram-new--v1.png" alt="instagram-new--v1"/></a>
-        </div>
-
-    </header>
 
     <div class="slider">
 
@@ -202,11 +171,7 @@
 
     </section>
 
-    <footer class="rodape">
-
-        <h3>Oryun Consultoria - O Apoio Essencial Para o Seu Negócio - Treinamento e Consultoria em Marketing</h3>
-
-    </footer>
+    <?php include 'src/componentes/footer.php'?>
 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     
---
+++ src/componentes/footer.php
@@ -0,0 +1,5 @@
+<footer class="rodape">
+
+        <h3>Oryun Consultoria - O Apoio Essencial Para o Seu Negócio - Treinamento e Consultoria em Marketing</h3>
+
+    </footer>
